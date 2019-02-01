a2d58ee84af4433713759f5feb9bb0ca201f333f
Zihan Tang <mrzihan.tang@gmail.com>
2019-02-01 15:16:04 +0800

    add nkn-check for relay counts

---
+++ nkn/roles/nkn-azure/files/reinstall.sh
@@ -0,0 +1,8 @@
+systemctl stop nkn
+cp config.json /nkn/
+cp wallet.dat /nkn/
+cp entrypoint.sh /nkn/
+cp nknd /usr/local/bin/
+cp nknc /usr/local/bin/
+cp nkn.service /etc/systemd/system/
+systemctl start nkn
